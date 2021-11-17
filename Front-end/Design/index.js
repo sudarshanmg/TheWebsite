10cb1166ab0a02754c11cb7244ff69dc8595d5bd
sudarshanmg <sudarshanmallibhat@gmail.com>
2021-11-17 10:29:43 +0530

    Second commit

---
+++ Front-end/Design/index.js
@@ -0,0 +1,8 @@
+// const mobile = window.matchMedia("(max-width: 900px)");
+//
+// mobile.addEventListener("change", () => {
+//     $("#remove").hide();
+//
+//     $("main").css("margin-right","1rem");
+//
+// });
